674c433b5197ee8b3ce694f609f487c89ebf7b58
shact.developer@gmail.com <shact.developer@gmail.com>
2015-12-08 14:32:13 -0200

    notfound page

--- client/lib/config/datatables.js
+++ client/lib/config/datatables.js
@@ -7,6 +7,7 @@ $.extend(true, $.fn.dataTable.defaults, {
     sProcessing: 'Processando....',
     sLengthMenu: 'Mostrar _MENU_ registros',
     sZeroRecords: 'Não foram encontrados resultados',
+    sEmptyTable: 'Não foram encontrados resultados',
     sInfo: 'Mostrando de _START_ até _END_ de _TOTAL_ registros',
     sInfoEmpty: 'Mostrando de 0 até 0 de 0 registros',
     sInfoFiltered: '',
